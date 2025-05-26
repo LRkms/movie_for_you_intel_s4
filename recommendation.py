f584c4ce0606bf799c91d09c2bbac5a7bcac5d87
LRkms <some0506@naver.com>
2025-05-26 11:58:04 +0900

    recommendation

--- recommendation.py
+++ recommendation.py
@@ -5,6 +5,7 @@ import pickle
 from konlpy.tag import Okt
 import re
 import datetime
+from gensim.models import Word2Vec
 
 def getRecommendations(cosine_sim):
     simScore = list(enumerate(cosine_sim[-1]))
@@ -20,10 +21,32 @@ tfidf_matrix = mmread('./models/tfidf_movie_review.mtx').tocsr()
 with open('./models/tfidf.pickle', 'rb') as f:
     tfidf = pickle.load(f)
 
-ref_idx = 745
-print(df_reviews.iloc[ref_idx, 0])
-cosine_sim = linear_kernel(tfidf_matrix[ref_idx], tfidf_matrix)
-print(cosine_sim)
+# 영화 index를 이용한 추천
+# ref_idx = 784
+# print(df_reviews.iloc[ref_idx, 0])
+# cosine_sim = linear_kernel(tfidf_matrix[ref_idx], tfidf_matrix)
+# print(cosine_sim)
+# print(len(cosine_sim[0]))
+# recommendation = getRecommendations(cosine_sim)
+# print(recommendation)
 
-recommendation = getRecommendations(cosine_sim)
-print(recommendation)
+# keyword를 이용한 추천
+embedding_model = Word2Vec.load('./models/word2vec_movie_review.model')
+keyword = '어벤져스'
+sim_word = embedding_model.wv.most_similar(keyword, topn=10)
+words = [keyword]
+for word, _ in sim_word:
+    words.append(word)
+sentence = []
+count = 10
+
+for word in words:
+    sentence = sentence + [word] * count
+    count -= 1
+sentence = ' '.join(sentence)
+print(sentence)
+
+sentence_vec = tfidf.transform([sentence])
+cosine_sim = linear_kernel(sentence_vec, tfidf_matrix)
+recommendations = getRecommendations(cosine_sim)
+print(recommendations)
